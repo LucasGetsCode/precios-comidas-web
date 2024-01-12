a6d26df191e041070325abf9dea3f6fa1689d476
LucasGetsCode <lucas.pujia@gmail.com>
2024-01-12 02:27:29 -0300

    coto

--- src/app/backend/descargar_precios.py
+++ src/app/backend/descargar_precios.py
@@ -12,9 +12,8 @@ import leer_articulos
 def carrefour(pagina):
     # precio = pagina.find_all("span", class_="valtech-carrefourar-dynamic-weight-price-0-x-container")
     # print(precio)
-    print(type(pagina))
     if type(pagina) != BeautifulSoup:
-        soup = BeautifulSoup(html.content, 'html.parser')
+        soup = BeautifulSoup(pagina.content, 'html.parser')
     else:
         soup = pagina
     print(type(soup))
@@ -22,8 +21,21 @@ def carrefour(pagina):
     for i in elementos:
         if i.has_attr("data-specification-value"):
             print(i['data-specification-value'])
-html = requests.get("https://www.carrefour.com.ar/jam-n-crudo-feteado-lario-x-120-grs/p")
-carrefour(html)
+
+def coto(pagina):
+    if type(pagina) != BeautifulSoup:
+        soup = BeautifulSoup(pagina.content, 'html.parser')
+    else:
+        soup = pagina
+    elemento = soup.find('span', class_="unit")
+    elemento.text
+    patron = r'\$([0-9]{1,3}(?:\.[0-9]{3})*,[0-9]{2})'
+    coincidencia = re.search(patron, elemento.text)
+    print(coincidencia.group(1))
+
+# coto(requests.get("https://www.cotodigital3.com.ar/sitios/cdigi/producto/-coca-cola-zero-15-lt/_/A-00179043-00179043-200"))
+# html = requests.get("https://www.carrefour.com.ar/jam-n-crudo-feteado-lario-x-120-grs/p")
+# carrefour(html)
 
 
 
